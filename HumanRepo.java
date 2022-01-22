62df993536402af14c170bdf78e8d6ad4f7dd91f
vishwa-developor <81308757+vishwa-developor@users.noreply.github.com>
2022-01-23 00:09:58 +0530

    Update HumanRepo.java

--- HumanRepo.java
+++ HumanRepo.java
@@ -6,5 +6,14 @@ import com.example.demo.model.Human;
 
 public interface HumanRepo extends CrudRepository<Human, Integer>
 {
-
+  List<Human> findByCity(String city);
+	
+	List<Human> findByCountry(String country);
+	
+	List<Human> findByIdGreaterThan(int id);
+	
+	
+	// Our Custom  JPQL QUERY
+	@Query("from Human where city=?1 order by name")
+	List<Human> findByCitySortedByName(String city);
 }
